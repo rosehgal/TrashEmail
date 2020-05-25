1f1fb4606cab1a443173e2a8967044988847e236
Rohit Sehgal <rohitsehgal1994@gmail.com>
2020-05-25 14:51:43 +0530

    Hopefully Fix #2; Update Async Telegram SendMessage
    This commit hopefully fixes #2, by making
    Telegram.sendMessage API call Async.

--- src/main/java/io/github/teledot/Telegram/SendTelegramMessage.java
+++ src/main/java/io/github/teledot/Telegram/SendTelegramMessage.java
@@ -6,18 +6,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@EnableAsync
 public class SendTelegramMessage {
     @Autowired
     private TelegramConfg telegramConfg;
 
     private static final Logger log = LoggerFactory.getLogger(SendTelegramMessage.class);
 
+    @Async
     public void sendMessage(String message, String chatId){
         String telegramURI = telegramConfg.getUrl() +
                              telegramConfg.getBotToken() +
