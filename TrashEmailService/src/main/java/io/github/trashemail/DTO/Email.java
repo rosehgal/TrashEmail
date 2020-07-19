ae6c0515ab43ad10e4f01d445933ec8bdad55340
Rohit Sehgal <rohitsehgal1994@gmail.com>
2020-07-19 11:59:00 +0530

    TrashEmail can receive emails from FetchMails :tada: :white_check_mark:

--- TrashEmailService/src/main/java/io/github/trashemail/DTO/Email.java
+++ TrashEmailService/src/main/java/io/github/trashemail/DTO/Email.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,10 +14,16 @@ public class Email {
     private String emailId;
     private String message;
     private Date arrived;
+    private String emailHostingLocation;
+    private String emailDownloadPath;
+    private List<String> attachmentsPaths;
 
     public Email(SendEmailRequest sendEmailRequest){
         this.arrived = new Date();
         this.emailId = sendEmailRequest.getEmailId();
         this.message = sendEmailRequest.getMessage();
+        this.emailHostingLocation = sendEmailRequest.getEmailHostingLocation();
+        this.emailDownloadPath = sendEmailRequest.getEmailDownloadPath();
+        this.attachmentsPaths = sendEmailRequest.getAttachmentsPaths();
     }
 }
