481f1c5d565f2aac0f7c3b970048039cbb9198ed
Thomas Lautaro López <laautarolopez@gmail.com>
2020-11-26 17:23:20 -0300

    EmailTest Finalizado

--- TrashEmailService/src/test/java/io/github/trashemail/DTO/EmailTest.java
+++ TrashEmailService/src/test/java/io/github/trashemail/DTO/EmailTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Arrays;
+
 public class EmailTest {
 	private Email email;
 	private SendEmailRequest sendEmailRequest;
@@ -25,5 +29,32 @@ public class EmailTest {
 		assertEquals(email.getEmailURI(), sendEmailRequest.getEmailURI());
 		assertEquals(email.getEmailDownloadPath(), sendEmailRequest.getEmailDownloadPath());
 		assertEquals(email.getAttachmentsPaths(), sendEmailRequest.getAttachmentsPaths());
+	}
+	
+	@Test
+	public void settersTest() {
+		Date date = mock(Date.class);
+		email.setArrived(date);
+		assertEquals(date, email.getArrived());
+		
+		String emailId = "hola@gmail.com";
+		email.setEmailId(emailId);
+		assertEquals(emailId, email.getEmailId());
+		
+		String message = "hola";
+		email.setMessage(message);
+		assertEquals(message, email.getMessage());
+		
+		String emailURI = "hola@gmail.com";
+		email.setEmailURI(emailURI);
+		assertEquals(emailURI, email.getEmailURI());
+		
+		String emailDownloadPath = "www.direccion.com";
+		email.setEmailDownloadPath(emailDownloadPath);
+		assertEquals(emailDownloadPath, email.getEmailDownloadPath());
+		
+		List<String> attachmentsPaths = Arrays.asList("at1", "at2", "at3");
+		email.setAttachmentsPaths(attachmentsPaths);
+		assertEquals(attachmentsPaths, email.getAttachmentsPaths());
 	}
 } 
